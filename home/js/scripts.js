3e2a869b7e31967d8717620d90b8e563ec57032a
jmaxeneds <jmaxeneds@gmail.com>
2025-01-10 18:43:48 +0800

    Update scripts.js

--- home/js/scripts.js
+++ home/js/scripts.js
@@ -6,3 +6,32 @@ document.querySelectorAll('.nav-menu a').forEach(anchor => {
       });
     });
   });
+
+// Get modal
+var modal = document.getElementById('imageModal');
+
+// Get all images in the portfolio
+var images = document.querySelectorAll('.portfolio-item img');
+
+// Get modal image element
+var modalImg = document.getElementById('modalImg');
+
+// Get modal caption element
+var captionText = document.getElementById('modalCaption');
+
+// Loop through all portfolio images
+images.forEach(function(image) {
+  image.onclick = function() {
+    modal.style.display = 'block'; // Show the modal
+    modalImg.src = this.src; // Set the image in the modal
+    captionText.innerHTML = this.alt; // Set the caption in the modal
+  };
+});
+
+// Get the close button element
+var closeBtn = document.getElementsByClassName('close')[0];
+
+// Close the modal when the close button is clicked
+closeBtn.onclick = function() {
+  modal.style.display = 'none';
+};
